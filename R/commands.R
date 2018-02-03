76786e1c6e7e098dde9ace333ead2015cef6db8c
Sebastian Kopf <seb.kopf@gmail.com>
2018-02-03 12:43:31 -0700

    update git commands

--- R/commands.R
+++ R/commands.R
@@ -1,131 +1,187 @@
 # non-git command functions ---
 
-ghc_copy_to_repositories <- function() {
+#' Copy a file or folder
+#' 
+#' Copy a file or folder to a number of repositories with the same prefix (typically those form the same assignment). Note that this function will overwrite existing files/folders that have the same name. It will also make sure that the copied file/folder is tracked by git (\code{\link{ghc_repos_git_add}}) unless parameter \code{git_add=FALSE}.
+#' 
+#' @inheritParams ghc_repos_get_local_information
+#' @param from the path to the file (or folder) that is to be copied to the respositories (absolute path or relative to the current working directory)
+#' @param to the relative path where in the target repositories the \code{from} file/folder should be copied to (default is the same name as the \code{from})
+#' @param git_add whether to make sure the copied file/folder is tracked by git
+#' @return returns the \code{prefix} invisibly to facilitate piping
+#' @family command functions
+#' @export
+ghc_repos_copy_to <- function(prefix, from, to = basename(from), folder = ".", git_add = TRUE) {
   
-}
-
-# general git command functions ---
-
-# Execute command in each folder
-# 
-# Convenience function to execute a bash command in a list of folders.
-# @param folders absolute paths or paths relative to current working directory
-exec_command <- function(folders, command, info = TRUE) {
-  map(folders, function(folder) {
-    if (info) message(glue("Folder: {folder}"))
-    glue("cd \"{folder}\" && {command}") %>% system()
+  folders <- suppressMessages(find_repositories(prefix, folder = folder))
+  if (!file.exists(from)) stop("'from' is not a valid file or folder", call. = FALSE)
+  
+  #info
+  is_dir <- dir.exists(from)
+  glue("Info: copying {if (is_dir) 'folder' else 'file'} '{from}' ",
+       "to '{file.path('[REPO]', to)}' ",
+       "{if (git_add) 'and adding it to git ' else ''}",
+       "for {length(folders)} repositories (prefix '{prefix}'):") %>% 
+    message()
+  
+  # folder files
+  if (is_dir)
+    from <- list.files(from, full.names = TRUE, recursive = TRUE)
+  
+  # copy files
+  map2(folders, 1:length(folders), function(folder, i) {
+    glue("Info: repository {i}/{length(folders)} ({folder})...") %>% message()
+    to_path <- file.path(folder, to)
+    if (is_dir && !dir.exists(to_path)) dir.create(to_path, recursive = TRUE)
+    file.copy(from = from, to = to_path, recursive = is_dir, overwrite = TRUE)
+    if (git_add) exec_command(folder, glue("git add -f \"{to}\""), info = FALSE)
   })
-  invisible(NULL)
+  
+  return(invisible(prefix))
 }
+
+# general git command functions ----
 
 #' Run a git command
 #' 
 #' Generic function to run a git command in a number of repositories with the same prefix (typically those form the same assignment).
 #' 
-#' @inheritParams ghc_find_local_repositories
+#' @inheritParams ghc_repos_get_local_information
 #' @param command a git command ("git" prefix will be automatically added if not present)
-#' @return returns the prefix invisibly to facilitate piping
+#' @return returns the \code{prefix} invisibly to facilitate piping
+#' @family command functions
 #' @export
-ghc_run_git_command <- function(prefix, command, folder = ".") {
+ghc_repos_run_git_command <- function(prefix, command, folder = ".") {
   if (missing(command)) stop("git command required", call. = FALSE)
-  folders <- suppressMessages(ghc_find_local_repositories(prefix, folder = folder))
+  folders <- suppressMessages(find_repositories(prefix, folder = folder))
   # make sure command has a git prefix
   command <- str_replace(command, "^git ", "") %>% str_c("git ", .)
   
   #info
-  glue("Info: executing command '{command}' for {length(folders)} git repositories (prefix '{prefix}'):") %>% 
+  glue("Info: executing command '{command}' for {length(folders)} repositories (prefix '{prefix}'):") %>% 
     message()
   exec_command(folders, command)
   return(invisible(prefix))
 }
 
-#' Run standard git commands
+#' Git Status
 #' 
-#' Run standard git commands for a set of student repositories (typically those from the same assignment). More specific commands can be executed with \code{\link{ghc_run_git_command}}.
+#' Shows the repository status in the active branch for all repositories that match the \code{prefix}.
 #' 
-#' @name ghc_commands
-#' @inheritParams ghc_find_local_repositories
-#' @return all standard command functions return the prefix invisibly to facilitate piping
-NULL
-
-#' @rdname ghc_commands
-#' @details \code{ghc_status} shows the repository status in the active branch for all repositories that match the \code{prefix}.
+#' @inherit ghc_repos_run_git_command params return
+#' @family command functions
 #' @export
-ghc_status <- function(prefix, folder = ".") {
-  ghc_run_git_command(prefix, command = "git status", folder = folder)
+ghc_repos_git_status <- function(prefix, folder = ".") {
+  ghc_repos_run_git_command(prefix, command = "git status", folder = folder)
 }
 
-#' @rdname ghc_commands
-#' @details \code{ghc_pull} pulls changes from the server to update the active branch for all repositories that match the \code{prefix}.
+#' Git Pull
+#' 
+#' Pulls changes from the server to update the active branch for all repositories that match the \code{prefix}.
+#' 
+#' @inherit ghc_repos_run_git_command params return
+#' @family command functions
 #' @export
-ghc_pull <- function(prefix, folder = ".") {
-  ghc_run_git_command(prefix, command = "git pull", folder = folder)
+ghc_repos_git_pull <- function(prefix, folder = ".") {
+  ghc_repos_run_git_command(prefix, command = "git pull", folder = folder)
 }
 
-#' @rdname ghc_commands
-#' @details \code{ghc_push} pushes committed changes in the active branch (see \code{ghc_commit}) for all repositories that match the \code{prefix} to GitHub. Note that this requires the repositories to have been cloned with a valid access token, otherwise this operation will fail due to lack of permissions. GitHub will also not allow \code{git push} if there are unpulled changes on the server. Use \code{ghc_pull} followed by \code{ghc_push} in this case (unless there are merging issues). 
+#' Git Push
+#' 
+#' Pushes committed changes in the active branch (see \code{\link{ghc_repos_git_commit}}) for all repositories that match the \code{prefix} to GitHub. Note that this requires the repositories to have been cloned with a valid access token, otherwise this operation will fail due to lack of permissions. GitHub will also not allow \code{git push} if there are unpulled changes on the server. Use \code{\link{ghc_repos_git_pull}} followed by \code{\link{ghc_repos_git_push}} in this case (unless there are merging issues). 
+#' 
+#' @inherit ghc_repos_run_git_command params return
+#' @family command functions
 #' @export
-ghc_push <- function(prefix, folder = ".") {
-  ghc_run_git_command(prefix, command = "git push", folder = folder)
+ghc_repos_git_push <- function(prefix, folder = ".") {
+  ghc_repos_run_git_command(prefix, command = "git push", folder = folder)
 }
 
-#' @rdname ghc_commands
-#' @details \code{ghc_commit} commits changes for all modified files in the active branch for all repositories that match the \code{prefix}. Use \code{ghc_add} and \code{ghc_remove} prior to \code{ghc_commit} to include new files (not tracked by git yet) / remove tracked files in the commit. Use \code{ghc_push} to push the committed changes back to the GitHub server.
+#' Git Commit
+#' 
+#' Commits changes for all modified files in the active branch for all repositories that match the \code{prefix}. Use \code{\link{ghc_repos_git_add}} and \code{\link{ghc_repos_git_remove}} prior to \code{\link{ghc_repos_git_commit}} to include new files (not tracked by git yet) / remove tracked files in the commit. Use \code{\link{ghc_repos_git_push}} to push the committed changes back to the GitHub server.
+#' 
+#' @inherit ghc_repos_run_git_command params return
+#' @family command functions
 #' @export
-ghc_commit <- function(prefix, message, folder = ".") {
+ghc_repos_git_commit <- function(prefix, message, folder = ".") {
   if (missing(message)) stop("commit message required", call. = FALSE)
-  ghc_run_git_command(prefix, command = glue("git commit -a -m \"{message}\" && git status"), folder = folder)
+  ghc_repos_run_git_command(prefix, command = glue("git commit -a -m \"{message}\" && git status"), folder = folder)
 }
 
-#' @rdname ghc_commands
-#' @details \code{ghc_add} adds the specified file to the active branch for all repositories that match the \code{prefix}. Use \code{ghc_commit} to commit added files, followed by \code{ghc_push} to push them to the server. Note that this function will always add the specific filed and overwrite .gitignore rules if necessary. You can also use this function to tag an existing file to be included in the next commit (this happens automatically for all modified files if you use the \code{ghc_commit} function to commit changes).
+#' Git Add
+#' 
+#' Adds the specified file to the active branch for all repositories that match the \code{prefix}. Use \code{\link{ghc_repos_git_commit}} to commit added files, followed by \code{\link{ghc_repos_git_push}} to push them to the server. Note that this function will always add the specific filed and overwrite .gitignore rules if necessary. You can also use this function to tag an existing file to be included in the next commit (this happens automatically for all modified files if you use the \code{\link{ghc_repos_git_commit}} function to commit changes).
+#' 
+#' @inherit ghc_repos_run_git_command params return
+#' @family command functions
 #' @param filepath new file to start tracking with git. The filepath is relative to the main repository directory within each repository.
 #' @export
-ghc_add <- function(prefix, filepath, folder = ".") {
+ghc_repos_git_add <- function(prefix, filepath, folder = ".") {
   if (missing(filepath)) stop("file path is required", call. = FALSE)
-  ghc_run_git_command(prefix, command = glue("git add -f \"{filepath}\" && git status"), folder = folder)
+  ghc_repos_run_git_command(prefix, command = glue("git add -f \"{filepath}\" && git status"), folder = folder)
 }
 
-#' @rdname ghc_commands
-#' @details \code{ghc_remove} removes the specified file from the active branch for all repositories that match the \code{prefix}. Use \code{ghc_commit} to commit removed files, followed by \code{ghc_push} to push the change to the server. 
-#' @inheritParams ghc_add
+#' Git Remove
+#' 
+#' Removes the specified file from the active branch for all repositories that match the \code{prefix}. Use \code{\link{ghc_repos_git_commit}} to commit removed files, followed by \code{\link{ghc_repos_git_push}} to push the change to the server. 
+#' 
+#' @inherit ghc_repos_run_git_command params return
+#' @inheritParams ghc_repos_git_add
+#' @family command functions
 #' @export
-ghc_remove <- function(prefix, filepath, folder = ".") {
+ghc_repos_git_remove <- function(prefix, filepath, folder = ".") {
   if (missing(filepath)) stop("file path is required", call. = FALSE)
-  ghc_run_git_command(prefix, command = glue("git rm \"{filepath}\" && git status"), folder = folder)
+  ghc_repos_run_git_command(prefix, command = glue("git rm \"{filepath}\" && git status"), folder = folder)
 }
 
-#' @rdname ghc_commands
-#' @details \code{ghc_create_branch} creates a new branch and switches to it for all repositories that match the \code{prefix}. Note that the git command used for this is \code{git checkout}. Also be warned that unless parameter \code{publish = FALSE}, the new branch is published to the GitHub remote right away.
+#' Git Create Branch
+#' 
+#' Creates a new branch and switches to it for all repositories that match the \code{prefix}. Note that the git command used for this is \code{git checkout}. Also be warned that unless parameter \code{publish = FALSE}, the new branch is published to the GitHub remote right away.
+#' 
+#' @inherit ghc_repos_run_git_command params return
 #' @param branch branch name (for commands that interact with branches)
-#' @param parent parent branch name or commit SHAE to use for creating the new branch (\code{ghc_create_branch} only)
-#' @param publish whether to publish the new branch, i.e. link it to the remote repository (\code{ghc_create_branch} only)
+#' @param parent parent branch name or commit SHAE to use for creating the new branch
+#' @param publish whether to publish the new branch, i.e. link it to the remote repository
+#' @family command functions
 #' @export
-ghc_create_branch <- function(prefix, branch, parent = "master", publish = TRUE, folder = ".") {
+ghc_repos_git_create_branch <- function(prefix, branch, parent = "master", publish = TRUE, folder = ".") {
   if (missing(branch)) stop("branch name required", call. = FALSE)
-  ghc_run_git_command(prefix, command = glue("git checkout -b {branch} {parent} && git push -u origin {branch}"), folder = folder)
+  ghc_repos_run_git_command(prefix, command = glue("git checkout -b {branch} {parent} && git push -u origin {branch}"), folder = folder)
 }
 
-#' @rdname ghc_commands
-#' @details \code{ghc_delete_branch} deletes an existing branch for all repositories that match the \code{prefix}. Be warned that with this function the branch is deleted both locally and on the server so be careful when using it. Can be followed by \code{ghc_create_branch} to recreate a branch with the same name but based on different commit (e.g. for updating an assignment `submit` branch).
-#' @inheritParams ghc_create_branch
+#' Git Delete Branch
+#' 
+#' Deletes an existing branch for all repositories that match the \code{prefix}. Be warned that with this function the branch is deleted both locally and on the server so be careful when using it. Can be followed by \code{\link{ghc_repos_git_create_branch}} to recreate a branch with the same name but based on different commit (e.g. for rebranching an assignment branch off a specific commit).
+#' 
+#' @inherit ghc_repos_run_git_command params return
+#' @inheritParams ghc_repos_git_create_branch
+#' @family command functions
 #' @export
-ghc_delete_branch <- function(prefix, branch, publish = TRUE, folder = ".") {
+ghc_repos_git_delete_branch <- function(prefix, branch, publish = TRUE, folder = ".") {
   if (missing(branch)) stop("branch name required", call. = FALSE)
-  ghc_run_git_command(prefix, command = glue("git branch -D {branch} && git push origin -d {branch}"), folder = folder)
+  ghc_repos_run_git_command(prefix, command = glue("git branch -D {branch} && git push origin -d {branch}"), folder = folder)
 }
 
-#' @rdname ghc_commands
-#' @details \code{ghc_switch_branch} lets you switch to a different branch that already exists for all repositories that match the \code{prefix}. Note that the git command used for this is \code{git checkout}.
-#' @inheritParams ghc_create_branch
+#' Git Switch Branch
+#' 
+#' Lets you switch to a different branch that already exists for all repositories that match the \code{prefix}. Note that the git command used for this is \code{git checkout}.
+#' 
+#' @inherit ghc_repos_run_git_command params return
+#' @inheritParams ghc_repos_git_create_branch
+#' @family command functions
 #' @export
-ghc_switch_branch <- function(prefix, branch = "master", folder = ".") {
-  ghc_run_git_command(prefix, command = glue("git checkout {branch}"), folder = folder)
+ghc_repos_git_switch_branch <- function(prefix, branch = "master", folder = ".") {
+  ghc_repos_run_git_command(prefix, command = glue("git checkout {branch}"), folder = folder)
 }
 
-#' @rdname ghc_commands
-#' @details \code{ghc_discard_changes} discards all unstaged(!) changes for repositories that match the \code{prefix}. This does NOT affect changes that have already been staged/committed (you would have to use \code{git reset} to do that). Note that this function uses the \code{git checkout} command, there is no \code{git discard}. 
+#' Git Discard Changes
+#' 
+#' Discards all unstaged(!) changes for repositories that match the \code{prefix}. This does NOT affect changes that have already been staged/committed (you would have to use \code{git reset} to do that). Note that this function uses the \code{git checkout} command, there is no \code{git discard}. 
+#' 
+#' @inherit ghc_repos_run_git_command params return
+#' @family command functions
 #' @export
-ghc_discard_changes <- function(prefix, folder = ".") {
-  ghc_run_git_command(prefix, command = "git checkout -- . && git status", folder = folder)
+ghc_repos_git_discard_changes <- function(prefix, folder = ".") {
+  ghc_repos_run_git_command(prefix, command = "git checkout -- . && git status", folder = folder)
 }
