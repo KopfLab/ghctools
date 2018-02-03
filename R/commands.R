5444889fecd58b6023bf1ca3ab2c077281e8a8f6
Sebastian Kopf <seb.kopf@gmail.com>
2018-02-03 12:53:44 -0700

    merge cloning and commands

--- R/commands.R
+++ R/commands.R
@@ -1,4 +1,6 @@
-# non-git command functions ---
+# repository commands ----
+
+# non-git command functions ----
 
 #' Copy a file or folder
 #' 
@@ -40,7 +42,7 @@ ghc_repos_copy_to <- function(prefix, from, to = basename(from), folder = ".", g
   return(invisible(prefix))
 }
 
-# general git command functions ----
+# general git command function ----
 
 #' Run a git command
 #' 
@@ -63,6 +65,98 @@ ghc_repos_run_git_command <- function(prefix, command, folder = ".") {
   exec_command(folders, command)
   return(invisible(prefix))
 }
+
+# cloning commands ----
+
+#' Clone assignment repositories
+#' 
+#' Clone all students' repositories for a specific assignment. Token with private repo access privileges is required if the students' repositories are private. This function can also be used to update existing local repositories for an assignment (i.e. after initial cloning if the students have made updates in the mean time). If updates are not desired, set \code{pull=TRUE}.
+#' 
+#' @inheritParams ghc_repos_get_github_information
+#' @inheritParams ghc_clone_repositories
+#' @inherit ghc_repos_run_git_command return
+#' @family command functions
+#' @return repositories data frame, see \link{ghc_repos_git_find_repositories} for details
+#' @export
+ghc_repos_git_clone <- function(prefix, org, folder = ".", token = NULL, pull = TRUE) {
+  
+  # security checks
+  if (missing(org)) stop("organization required", call. = FALSE)
+  if (missing(prefix)) stop("assignment prefix required", call. = FALSE)
+  
+  # find repositories
+  ghc_repos_get_github_information(org = org, prefix = prefix, token = token) %>% 
+    # clone repos
+    ghc_clone_repositories(folder = folder, token = token, pull = pull)
+  
+  return(invisible(prefix))
+}
+
+
+#' Clone repositories
+#' @inheritParams ghc_clone_repository
+#' @param repos data frame with, at minimum, columns 'repository' (name) and 'url'
+#' @param folder the target directory where to clone all the repositories to
+#' @return the \code{repos} data frame (unaltered) for easy piping
+ghc_clone_repositories <- function(repos, folder = ".", token = NULL, pull = TRUE) {
+  if (missing(repos)) stop("repos data frame required", call. = FALSE)
+  if (nrow(repos) > 0) {
+    if (!"url" %in% names(repos)) stop("missing column 'url'", call. = FALSE)
+    if (!"repository" %in% names(repos)) stop("missing column 'repos'", call. = FALSE)
+    pmap(
+      list(url = repos$url, 
+           path = file.path(folder, repos$repository), 
+           token = token, 
+           pull = pull
+      ),
+      ghc_clone_repository)
+  }
+  return(invisible(repos))
+}
+
+#' Clone a repository to the target directory
+#' @param url the https://... repository 
+#' @param path the directory to clone to, if not provided, clones into the current working directory with the repository name
+#' @param token the authentication token (only required for private repositories if global credentials are not set)
+#' @param pull if the repository already exists, whether to update it from the remote (see \code{\link{ghc_repos_git_pull}})
+#' @return whether the repository was cloned/pulled
+ghc_clone_repository <- function(url, path = NULL, token = NULL, pull = TRUE) {
+  
+  # token
+  if (!is.null(token)) url <- str_c("https://", token, "@", str_replace(url, "^https://", "")) 
+  if (is.null(path)) path <- basename(url)
+  
+  # parent directory
+  if (!dir.exists(dirname(path)))  {
+    message("Info: creating parent directory: ", dirname(path))
+    dir.create(dirname(path), recursive = TRUE)
+  } 
+  
+  # different cases
+  folder <- basename(path)
+  if (!dir.exists(path)) {
+    message(glue("Info: '{folder}' - cloning repository for the first time"))
+    cmd_folder <- "."
+    cmd <- glue("git clone \"{url}\" \"{path}\"")
+  } else if (dir.exists(path) && !dir.exists(file.path(path, ".git"))) {
+    message(glue("Info: '{folder}' - directory already exists but is NOT a github repository -> no action"))
+    return(FALSE)
+  } else if (dir.exists(path) && pull) {
+    message(glue("Info: '{folder}' - repository already exists -> pulling changes from remote"))
+    cmd_folder <- path
+    cmd <- "git pull"
+  } else {
+    message(glue("Info: '{folder}' - repository already exists but NOT pulling changes from remote -> no action"))
+    return(FALSE)
+  } 
+  
+  # run github call
+  exec_command(cmd_folder, cmd, info = FALSE, output = TRUE)
+  return(TRUE)
+}
+
+
+# other common git commands ----
 
 #' Git Status
 #' 
--- tests/testthat/test-commands.R
+++ tests/testthat/test-commands.R
@@ -2,13 +2,24 @@ context("Command functions")
 
 test_that("Test command functions errors", {
   
+  # relevant utils
   expect_error(find_repositories(), "prefix required")
   expect_message(find_repositories("test", folder = system.file(package = "ghctools")),
                  "found 0 git repositories")
   
+  # general command
   expect_error(ghc_repos_run_git_command(command = "test"), "prefix required")
   expect_error(ghc_repos_run_git_command("test"), "command required")
   
+  # cloning 
+  expect_error(ghc_clone_repositories(), "repos data frame required")
+  expect_error(ghc_clone_repositories(data_frame(x = 1)), "missing column")
+  expect_error(ghc_clone_repositories(data_frame(url = "url")), "missing column")
+  expect_error(ghc_clone_repositories(data_frame(repository = "repo")), "missing column")
+  expect_error(ghc_repos_git_clone(prefix = "test"), "organization required")
+  expect_error(ghc_repos_git_clone(org = "test"), "prefix required")
+  
+  # other standard commands
   expect_error(ghc_repos_git_status(), "prefix required")
   expect_error(ghc_repos_git_pull(), "prefix required")
   expect_error(ghc_repos_git_push(), "prefix required")
@@ -26,3 +37,5 @@ test_that("Test command functions errors", {
   expect_error(ghc_repos_git_discard_changes(), "prefix required")
   
 })
+
+
