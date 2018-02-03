f83054af164453fd73a040dbdb708431e3c03c16
Sebastian Kopf <seb.kopf@gmail.com>
2018-02-03 13:14:52 -0700

    package imports

---
+++ R/package.R
@@ -0,0 +1,31 @@
+#' @keywords internal
+"_PACKAGE"
+
+#' @importFrom glue glue collapse
+#' @importFrom dplyr select rename mutate data_frame tbl_df matches everything
+#' @importFrom purrr pmap map_chr map_lgl
+#' @importFrom lubridate ymd_hms
+#' @importFrom stringr str_c str_interp str_replace
+#' @import ghql
+#' @import httr
+NULL
+
+#' @importFrom magrittr %>%
+#' @export
+magrittr::`%>%`
+
+# standard OR function
+`%||%` <- function (a, b) {
+  if (!is.null(a)) a else b
+}
+
+# quiets concerns of R CMD check about . that appears in pipelines 
+# and some very commonly used variable names used in NSE commands
+utils::globalVariables(c("."))
+
+# release questions 
+release_questions <- function() {
+  c(
+    "Is it passing travis, appveyor and win-builder?"
+  )
+}
