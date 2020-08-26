dce3f69be135fb7a28f41c0efe37673b11206f44
Sebastian Kopf <sebkopf>
2020-08-25 21:17:00 -0600

    add tests

--- tests/testthat/test-information.R
+++ tests/testthat/test-information.R
@@ -5,5 +5,6 @@ test_that("Test information functions errors", {
   expect_error(ghc_repos_get_github_information(prefix = "test"), "organization required")
   expect_error(ghc_repos_get_github_information(org = "test"), "assignment prefix required")
   expect_error(ghc_repos_get_github_information("test", "test"), "access token required")
+  expect_error(ghc_repos_get_github_information("test", "test", token = "DNE"), "invalid access token")
   
 })
---
+++ tests/testthat/test-scenario.R
@@ -0,0 +1,7 @@
+context("Scenario")
+
+test_that("Test full scenario of github interaction", {
+  
+  expect_true(TRUE)
+  
+})
